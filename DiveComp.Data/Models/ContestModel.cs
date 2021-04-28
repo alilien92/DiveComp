9d226ba4edc1b3e36689b39600b1225b8463ac50
Pontus Wallquist <pontuswallquist@gmail.com>
2021-04-28 12:33:26 +0200

    testing  commit23

---
+++ DiveComp.Data/Models/ContestModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DiveComp.Data.Models
+{
+    public class ContestModel
+    {
+        public int contestId { get; set; }
+
+        public ScoreboardModel scoreboard { get; set; }
+
+    }
+}
---
+++ DiveComp.Data/Models/testme.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DiveComp.Data.Models
+{
+    public class testme
+    {
+        public string sex;
+        public int age;
+    }
+}
